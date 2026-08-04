562d9d379d54b482c0b61ed948850161c717ee6f
Nirmal Akarsha <nirmalakarsha74@gmail.com>
2026-08-04 09:27:41 +0530

    Refactor Student class to use abstract methods

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
-class Student:
+from abc import ABC, abstractclassmethod
+class Student(ABC):
     def __init__(self, stId, stName, stAge, stDegreeProgram ):
         self.StId = stId
         self.StName = stName
@@ -6,7 +7,50 @@ class Student:
         self.StDegreeProgram = stDegreeProgram
         self.StRgCourses = []
 
+    @abstractclassmethod
+    def st_registration(self):
+        pass
+
+    @abstractclassmethod
+    def st_details(self):
+        pass
+
     def student_course_registration(self, *courses):
 
         for course in courses:
             self.StRgCourses.append(course)
+
+class DegreeStudent(Student):
+    def __init__(self, stId, stName, stAge, stProgram, degreeFees):
+        super().__init__(stId, stName, stAge, stProgram)
+        self.degreeFees = degreeFees
+
+    def student_registration(self):
+        self.StName = input("Student Name:")
+        self.StId = int(input("Enter Student ID:"))
+        self.StAge = int(input("Age:"))
+        self.StProgram = input("What is the program student registered:")
+
+    def st_allDetails(self):
+        print("----Degree programs student details----")
+        print(f"Student ID: {self.StId}")
+        print(f"Student Name: {self.StName}")
+        print(f"Student Age: {self.StAge}")
+        print(f"Student Degree program: {self.StProgram}")
+
+class CourseStudent(Student):
+    def __init__(self, stId, stName, stAge, stProgram):
+        super().__init__(stId, stName, stAge, stProgram)
+
+    def student_registration(self):
+            self.StName = input("Student Name:")
+            self.StId = int(input("Enter Student ID:"))
+            self.StAge = int(input("Age:"))
+            self.StProgram = input("What is the program student registered:")
+    
+    def st_allDetails(self):
+            print("----Course programs student details----")
+            print(f"Student ID: {self.StId}")
+            print(f"Student Name: {self.StName}")
+            print(f"Student Age: {self.StAge}")
+            print(f"Student Course  program: {self.StProgram}")
